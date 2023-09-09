9488fe6e1eb95ac426d2758ccca50b185a431625
Eatowl <csnelcov@gmail.com>
2023-09-09 17:06:44 +0300

    Fix code style

--- src/stereo_camera/main.py
+++ src/stereo_camera/main.py
@@ -3,11 +3,12 @@
 
 # A simple code snippet
 # Using two  CSI cameras (such as the Raspberry Pi Version 2) connected to a
-# NVIDIA Jetson Nano Developer Kit with two CSI ports (Jetson Nano, Jetson Xavier NX) via OpenCV
+# NVIDIA Jetson Nano Developer Kit with two CSI ports (Jetson Nano, Jetson Xavier NX)
+# via OpenCV
 # Drivers for the camera and OpenCV are included in the base image in JetPack 4.3+
 
-# This script will open a window and place the camera stream from each camera in a window
-# arranged horizontally.
+# This script will open a window and place the camera stream
+# from each camera in a window arranged horizontally.
 # The camera streams are each read in their own thread, as when done sequentially there
 # is a noticeable lag
 
@@ -31,7 +32,7 @@ class Camera:
                 gstreamer_pipeline_string, cv2.CAP_GSTREAMER
             )
             
-            if self.video_capture != None:
+            if self.video_capture is not None:
                 self.running = True
                 self.read_thread = threading.Thread(target=self.updateFrame)
                 self.read_thread.start()
@@ -60,10 +61,10 @@ class Camera:
         self.read_thread = None
 
     def release(self):
-        if self.video_capture != None:
+        if self.video_capture is not None:
             self.video_capture.release()
             self.video_capture = None
-        if self.read_thread != None:
+        if self.read_thread is not None:
             self.read_thread.join()
         
 
@@ -93,30 +94,43 @@ class Stereo_Camera():
         if self.left.video_capture.isOpened() and\
             self.right.video_capture.isOpened():
             return True
-        else: return False
+        else: 
+            return False
 
     def loadCalibConfigs(self, extrinsics_path='config/extrinsics.yml',
                                intrinsics_path='config/intrinsics.yml'):
         extrinsics_config = cv2.FileStorage(extrinsics_path, cv2.FILE_STORAGE_READ)
         intrinsics_config = cv2.FileStorage(intrinsics_path, cv2.FILE_STORAGE_READ)
 
-        return extrinsics_config.getNode("R").mat(), extrinsics_config.getNode("T").mat(),\
-               extrinsics_config.getNode("R1").mat(), extrinsics_config.getNode("R2").mat(),\
-               extrinsics_config.getNode("P1").mat(), extrinsics_config.getNode("P2").mat(),\
-               extrinsics_config.getNode("Q").mat(), intrinsics_config.getNode("M1").mat(),\
-               intrinsics_config.getNode("D1").mat(), intrinsics_config.getNode("M2").mat(),\
+        return extrinsics_config.getNode("R").mat(),\
+               extrinsics_config.getNode("T").mat(),\
+               extrinsics_config.getNode("R1").mat(),\
+               extrinsics_config.getNode("R2").mat(),\
+               extrinsics_config.getNode("P1").mat(),\
+               extrinsics_config.getNode("P2").mat(),\
+               extrinsics_config.getNode("Q").mat(),\
+               intrinsics_config.getNode("M1").mat(),\
+               intrinsics_config.getNode("D1").mat(),\
+               intrinsics_config.getNode("M2").mat(),\
                intrinsics_config.getNode("D2").mat()
 
     def calibCameras(self, left_image, right_image):
-        R, T, R1, R2, P1, P2, Q, M1, D1, M2, D2 = self.loadCalibConfigs('config/extrinsics.yml',
-                                                                        'config/intrinsics.yml')
-        if self.left != None:
+        R, T, R1, R2, P1, P2, \
+        Q, M1, D1, M2, D2 = self.loadCalibConfigs('config/extrinsics.yml',
+                                                  'config/intrinsics.yml')
+        if self.left is not None:
             height, width, channel = left_image.shape
 
-            leftMapX, leftMapY = cv2.initUndistortRectifyMap(M1, D1, R1, P1, (width, height), cv2.CV_32FC1)
-            left_rectified = cv2.remap(left_image, leftMapX, leftMapY, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
-            rightMapX, rightMapY = cv2.initUndistortRectifyMap(M2, D2, R2, P2, (width, height), cv2.CV_32FC1)
-            right_rectified = cv2.remap(right_image, rightMapX, rightMapY, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
+            leftMapX, leftMapY = cv2.initUndistortRectifyMap(M1, D1, R1, P1, 
+                                                            (width, height), 
+                                                             cv2.CV_32FC1)
+            left_rectified = cv2.remap(left_image, leftMapX, leftMapY, 
+                                       cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
+            rightMapX, rightMapY = cv2.initUndistortRectifyMap(M2, D2, R2, P2, 
+                                                              (width, height), 
+                                                               cv2.CV_32FC1)
+            right_rectified = cv2.remap(right_image, rightMapX, rightMapY, 
+                                        cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
 
             return left_rectified, right_rectified
         else:
@@ -138,12 +152,12 @@ def gstreamer_pipeline(
     framerate=30,
     flip_method=0,
 ):
-    return "nvarguscamerasrc sensor-id=%d ! " \
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! " \
-        "nvvidconv flip-method=%d ! " \
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! " \
-        "videoconvert ! " \
-        "video/x-raw, format=(string)BGR ! appsink" \
+    return "nvarguscamerasrc sensor-id=%d ! "\
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d,"\
+        "framerate=(fraction)%d/1 ! nvvidconv flip-method=%d ! " \
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "\
+        "videoconvert ! "\
+        "video/x-raw, format=(string)BGR ! appsink"\
         % (
             sensor_id,
             capture_width,
